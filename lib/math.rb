daed1019941790251f3f96a22c689af65087f8ce
Sara McCombs <thesaramccombs@gmail.com>
2019-07-26 02:48:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,26 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  puts sum
 end
 
 def subtraction(num1, num2)
-
+  difference = num2 - num1 
+  puts difference
 end
 
 def division(num1, num2)
-
+  divide = num2/num1
+  puts divide
 end
 
 def multiplication(num1, num2)
-
+  multiply = num1 * num2
+  puts multiply
 end
 
 def modulo(num1, num2)
-
+  remainder = num2 % num1
+  puts remainder
 end
 
 def square_root(num)
